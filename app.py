3511c39740b6803efea84c707a56aec5d091cb30
Amina Masera <aminakavele@gmail.com>
2026-09-13 18:52:06 +0300

    Update 2

--- app.py
+++ app.py
@@ -19,25 +19,36 @@ events = [
 
 @app.route("/", methods=["GET"])
 def get_events():
+    
     print (events)
-    event = (eachevent for eachevent in events )
-    print (event)
-    return jsonify([event.to_dict()]), 200
+    for eachevent in events:
+        print(eachevent)
+        test = eachevent.to_dict()
+        print(test)
+        output = {
+        "data": test
+        }
+        return jsonify([output])
+
 
 # Create a new event from JSON input
 @app.route("/events", methods=["POST"])
 def create_event():
     data = request.get_json()
-    
+
     Title = data["title"]
     event_id = len(events) + 1
 
     new_event = Event(id = event_id, title = Title)
 
     events.append(new_event)
-    print(new_event.to_dict())
 
-    return (new_event.to_dict()), 201
+    output = {"date": {
+        "id": new_event.id,
+        "title": new_event.title
+    }}
+
+    return jsonify(output)
 
 # Update the title of an existing event
 @app.route("/events/<int:event_id>", methods=["PATCH"])
@@ -48,7 +59,7 @@ def update_event(event_id):
     find_event = next((test for test in events if test.id == event_id))
 
     if not find_event:
-        return ("error, not found", 404)
+        return ("error, not found"), 404
     if "title" in data:
         find_event.title = Title
         
